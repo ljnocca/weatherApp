7cfa1f0527787940da222a73d3f03387e6c5b563
Laurent Nocca <ljnocca@gmail.com>
2017-02-26 13:47:13 -0600

    Sunday first draft

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 var baseURL = 'https://api.darksky.net/forecast/f693ad4fa47137321f70f403e91be488/'
+var googleAPI = 'http://maps.googleapis.com/maps/api/geocode/json?address='
+
 var navNode = document.querySelector('.navBar')
 navigator.geolocation.getCurrentPosition(handleCoords)
+var searchNode = document.querySelector('.searchBar')
+
+
+//**************************CONTROLLER**************************
+function controller() {
+    var hashStr = location.hash.substr(1)
+    var hashParts = hashStr.split('/')
+
+    var latitude = hashParts[0]
+    var longitude = hashParts[1]
+    var viewType = hashParts[2]
+
+    var promiseInput = baseURL+latitude+','+longitude+'?callback=?'
+    var weatherPromise = $.getJSON(promiseInput)
+
+    if (hashParts.length < 3) {
+        (handleCoords, handleError)
+        return // leave the controller. the controller will run again when handleCoords causes another hashchange
+    }
+
+    if (viewType === 'current') {
+        weatherPromise.then(handleCurrent)
+    } else if (viewType === 'daily') {
+        weatherPromise.then(handleDaily)
+    } else if (viewType === 'hourly') {
+        weatherPromise.then(handleHourly)
+    }
+}
 
 //**************************HANDLECOORDS: CREATES HASH PATH**************************
 function handleCoords (coordsObj) { 
@@ -14,21 +44,82 @@ function handleCoords (coordsObj) {
     })
 }
 
+
+searchNode.addEventListener('keydown',function (event){
+	if (event.keyCode === 13){
+		var city = event.target.value
+		
+		var firstLetter = city.substring(0,1).toUpperCase()
+		var capitalizedCity = firstLetter + city.substring(1,city.length)
+		var pageName = document.querySelector('#pageName')
+		pageName.innerHTML = '<h1>'+ capitalizedCity + " weather</h1>"
+		
+		var googlePromise = $.getJSON(googleAPI+city)
+		googlePromise.then(handleCity)
+		event.target.value = ''
+	}
+})
+		
+function handleCity (apiResponse){
+	var lat = apiResponse.results[0].geometry.location.lat
+	var lng = apiResponse.results[0].geometry.location.lng
+	window.location.hash = lat + '/' + lng + '/current'
+
+	navNode.addEventListener('click',function(event){
+    	window.location.hash = lat + '/' + lng + event.target.value
+    })
+}
+
+
+
 function handleError(err) {//this function is only run if an error is detected
     console.log('Error!', err)
 }
 
+//*******************************************************************
+//								VIEWS
+//*******************************************************************
 
-//**************************CUURENT WEATHER**************************
+//**************************CURRENT WEATHER**************************
 function handleCurrent(currentWeather) {
+	var htmlString = ''
     var containerNode = document.querySelector('.weatherContainer')
-    containerNode.innerHTML += '<h1>Current temperature: ' + currentWeather.currently.temperature+ ' &#8457</h1>'
-    containerNode.innerHTML += '<h4>Feels like : ' + currentWeather.currently.apparentTemperature+ ' &#8457</h4>'
-    containerNode.innerHTML += '<h2>'+ currentWeather.currently.summary+ '</h2>'
-    containerNode.innerHTML += '<h5>Chance of rain: '+ currentWeather.currently.precipProbability * 100 + ' %</h5>'
+    htmlString += '<div class="innerDiv">'
+    htmlString +=	'<h1>NOW</h1>'
+    htmlString += 	'<h1>' + currentWeather.currently.temperature+ ' &#8457</h1>'
+    htmlString += 	'<h2>'+ currentWeather.currently.summary+ '</h2>'
+    htmlString += 	'<h4>Feels like ' + currentWeather.currently.apparentTemperature+ ' &#8457</h4>'
+    htmlString += 	'<h4>'+ currentWeather.currently.precipProbability * 100 + '% chance of rain</h5>'
+    htmlString += '</div>'
+    containerNode.innerHTML= htmlString
 }
 
 //**************************DAILY WEATHER**************************
+
+function handleDaily(arrayOfObjects){
+	var arrayToHTML = ''
+	var dailyArray = arrayOfObjects.daily.data
+	var containerNode = document.querySelector('.weatherContainer')
+	for (var i = 0; i<dailyArray.length; i++){
+		arrayToHTML += dailyHTML(dailyArray[i])
+	}
+	containerNode.innerHTML = arrayToHTML
+}
+
+function dailyHTML(dailyWeather) {
+    var htmlString = ''
+    htmlString += '<div class="innerDiv">'
+    htmlString += 	'<h3><u>' + dayConverter(dailyWeather.time)+ '</u></h3>'
+    htmlString += 	'<h4>Temp: ' + dailyWeather.temperatureMax+ ' / ' + dailyWeather.temperatureMin +  '&#8457</h4>'
+    htmlString += 	'<h4>'+ dailyWeather.summary+ '</h4>'
+    htmlString += 	'<h4>Chance of rain: '+ dailyWeather.precipProbability * 100 + ' %</h4>'
+    htmlString += '</div>'
+    var divNode = document.querySelector('.innerDiv')
+    divNode.style.display = 'inline-block'
+    divNode.style.width = '32%'
+    return htmlString
+}
+
 function dayConverter (unixTime){
   	var date = new Date(unixTime*1000);
 	var day = date.getDay();
@@ -49,63 +140,54 @@ function dayConverter (unixTime){
     }
 }
 
-function dailyHTML(dailyWeather) {
-    var htmlString = ''
-    htmlString += '<h1>Day: ' + dayConverter(dailyWeather.time)+ '</h1>'
-    htmlString += '<h1>Max temperature: ' + dailyWeather.temperatureMax+ ' &#8457</h1>'
-    htmlString += '<h1>Min temperature: ' + dailyWeather.temperatureMin + ' &#8457</h1>'
-    htmlString += '<h2>Chance of rain: '+ dailyWeather.precipProbability * 100 + ' %</h2>'
+//**************************HOURLY WEATHER**************************
+function hourConverter(timestamp) {
+  var d = new Date(timestamp * 1000),
+		hh = d.getHours(),
+		h = hh,
+		min = ('0' + d.getMinutes()).slice(-2),
+		ampm = 'AM',
+		time;
+			
+	if (hh > 12) {
+		h = hh - 12;
+		ampm = 'PM';
+	} else if (hh === 12) {
+		h = 12;
+		ampm = 'PM';
+	} else if (hh == 0) {
+		h = 12;
+	}
+	time = h + ':' + min + ' ' + ampm;	
+	return time;
+}
+
+function hourlyHTML(hourlyWeather) {
+	var htmlString = ''
+	htmlString += '<div class="innerDiv">'
+    htmlString += 	'<h1>' + hourConverter(hourlyWeather.time) + '</h1>'
+    htmlString += 	'<h1>Temperature: ' + hourlyWeather.temperature + ' &#8457</h1>'
+    htmlString += 	'<h2>'+ hourlyWeather.summary+ '</h2>'
+    htmlString += 	'<h2>Chance of rain: '+ hourlyWeather.precipProbability * 100 + ' %</h2>'
+    htmlString += '</div>'
     return htmlString
 }
 
-function handleDaily(arrayOfObjects){
+function handleHourly(arrayOfObjects){
 	var arrayToHTML = ''
-	var dailyArray = arrayOfObjects.daily.data
+	var hourlyArray = arrayOfObjects.hourly.data
 	var containerNode = document.querySelector('.weatherContainer')
-	for (var i = 0; i<dailyArray.length; i++){
-		arrayToHTML += dailyHTML(dailyArray[i])
+	for (var i = 0; i<hourlyArray.length; i++){
+		arrayToHTML += hourlyHTML(hourlyArray[i])
 	}
 	containerNode.innerHTML = arrayToHTML
 }
 
-//**************************HOURLY WEATHER**************************
-function handleHourly(hourlyWeather) {
-    var containerNode = document.querySelector('.weatherContainer')
-    containerNode.innerHTML = '<p>gonna show you the hourly weather</p>'
-}
 
-//**************************CONTROLLER**************************
-function controller() {
-    var hashStr = location.hash.substr(1)
-    var hashParts = hashStr.split('/')
 
-    var latitude = hashParts[0]
-    var longitude = hashParts[1]
-    var viewType = hashParts[2]
 
-    var promiseInput = baseURL+latitude+','+longitude+'?callback=?'
-    var weatherPromise = $.getJSON(promiseInput)
-
-    if (hashParts.length < 3) {
-        // if there is not enough information currently in the hash,
-        // then get the user's current location, and write 
-        // a new hash accordingly
-        (handleCoords, handleError)
-        return // leave the controller. the controller will run again when
-        // handleCoords causes another hashchange
-    }
-
-    if (viewType === 'current') {
-        weatherPromise.then(handleCurrent)
-    } else if (viewType === 'daily') {
-        weatherPromise.then(handleDaily)
-    } else if (viewType === 'hourly') {
-        weatherPromise.then(handleHourly)
-    }
-}
-
+//*******************************************************************
+//				RUN CONTROLLER INITIALLLY & ON HASH CHAHGE
+//*******************************************************************
 window.addEventListener('hashchange', controller)
-
 controller()
-
-
